ad2eefee634d96223a1abac3a0fdc92bd97b6e5e
Shalini8 <shalu8mar@gmail.com>
2021-04-08 23:54:23 +0530

    welcome message

---
+++ Welcome.java
@@ -0,0 +1,12 @@
+package javapractice;
+
+public class snakeuc1 {
+	
+		public static void main(String args[])
+		{
+			System.out.println("Welcome to snake and ladder game!");
+			
+			
+		
+		}
+	}
